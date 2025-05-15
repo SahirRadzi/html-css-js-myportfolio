86696649b98c53c5b6bedea2f3b66bf6aa6fcff9
SahirRadzi <sahir.radzi@gmail.com>
2025-05-15 11:50:47 +0800

    update experience & projects

--- visitg.js
+++ visitg.js
@@ -3,3 +3,25 @@ function visitGithub() {
   // Membuka tab baru Github
   window.open("https://github.com/SahirRadzi", "_blank");
 }
+
+// Recent Projects
+// Function LiveMonsta
+function LiveMonsta() {
+  // Open new tab
+  window.open("https://sahirradzi.github.io/monsta-web/", "_blank");
+}
+// Function RepoMonsta
+function RepoMonsta() {
+  // Open new tab
+  window.open("https://github.com/SahirRadzi/monsta-web", "_blank");
+}
+// Function LiveWarnakala
+function LiveWarnakala() {
+  // Open new tab
+  window.open("https://sahirradzi.github.io/warnakala-web/", "_blank");
+}
+// Function RepoWarnakala
+function RepoWarnakala() {
+  // Open new tab
+  window.open("https://github.com/SahirRadzi/warnakala-web", "_blank");
+}
